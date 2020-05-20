11c82d1fdd0071a960c96bb5bde61b721d711bc2
Mupa1 <mupasmail@gmail.com>
2020-05-20 14:59:26 +0200

    refactor code

--- bin/main.rb
+++ bin/main.rb
@@ -1,90 +1,71 @@
 #!/usr/bin/env ruby
 # rubocop:disable Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
-require_relative '../lib/wins.rb'
 require_relative '../lib/players.rb'
 require_relative '../lib/game.rb'
 
-class PlayGame
-  include Game
+include Util
 
-  def initialize
-    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-    @playerz = Players.new
+@game = Game.new
+@playerz = Players.new
+
+def welcome
+  puts 'Welcome to Tic Tac Toe'
+  puts 'Please Enter your name (Player 1): '
+  player1_name = gets.strip.upcase
+  until @playerz.validate_name(player1_name)
+    puts 'Player 1: Enter Valid name'
+    player1_name = gets.chomp.upcase
   end
-
-  def welcome
-    puts 'Welcome to Tic Tac Toe'
-    puts 'Please Enter your name (Player 1): '
-    player1_name = gets.strip.upcase
-    until @playerz.validate_name(player1_name)
-      puts 'Player 1: Enter Valid name'
-      player1_name = gets.chomp.upcase
-    end
-    puts 'Please Enter your name (Player 2): '
-    player2_name = gets.strip.upcase
-    until @playerz.validate_name(player2_name)
-      puts 'Player 2: Enter Valid name'
-      player2_name = gets.chomp.upcase
-    end
-    @playerz.player(player1_name, player2_name)
-    puts "#{player1_name} is 'X' (player1) and #{player2_name} is 'O' (player2)"
+  puts 'Please Enter your name (Player 2): '
+  player2_name = gets.strip.upcase
+  until @playerz.validate_name(player2_name)
+    puts 'Player 2: Enter Valid name'
+    player2_name = gets.chomp.upcase
   end
+  @playerz.player(player1_name, player2_name)
+  puts "#{player1_name} is 'X' (player1) and #{player2_name} is 'O' (player2)"
+end
 
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts '----------- '
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts '----------- '
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
-
-  def play
-    welcome
-    wins = Wins.new
-    game_over = false
-    display_board
-    until game_over == true
-      puts 'Select a number between 1-9'
-      input = input_to_index(gets.chomp)
-      if !valid_move?(@board, input)
-        puts 'Invalid move, number already taken or out of range'
-      else
-        move(@board, input, current_player)
-        if wins.won?(@board, 'X')
-          display_board
+def play
+  welcome
+  game_over = false
+  puts @game.display_board
+  until game_over == true
+    puts 'Select a number between 1-9'
+    input = input_to_index(gets.chomp)
+    if !@game.valid_move?(input)
+      puts 'Invalid move, number already taken or out of range'
+    else
+      @game.move(input, @game.current_player)
+      if @game.won?
+        if @game.current_player == 'O'
+          puts @game.display_board
           puts "#{@playerz.player1} won the game!"
-          return game_over = true
-        elsif !@board.include?(' ')
-          display_board
-          puts 'It\'s a Draw!'
-          return game_over = true
-        end
-        if wins.won?(@board, 'O')
-          display_board
+          return game_over == true
+        else
+          puts @game.display_board
           puts "#{@playerz.player2} won the game!"
-          return game_over = true
-        elsif !@board.include?(' ')
-          display_board
-          puts 'It\'s a Draw!'
-          return game_over = true
+          return game_over == true
         end
+      elsif !@game.board.include?(' ')
+        puts @game.display_board
+        puts 'It\'s a Draw!'
+        return game_over == true
       end
-      display_board
     end
-  end
-
-  def play_again
-    play = PlayGame.new
-    game_over = true
-    puts 'Would you like to restart the game (Yes/No)'
-    player_response = gets.chomp.downcase
-    return game_over if player_response != 'yes'
-
-    play.play
-    play.play_again
+    puts @game.display_board
   end
 end
+play
+
+def play_again
+  game_over = true
+  puts 'Would you like to restart the game (Yes/No)'
+  player_response = gets.chomp.downcase
+  return game_over if player_response != 'yes'
+  @game.empty_board
+  play
+  play_again
+end
+play_again
 # rubocop:enable Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
-game = PlayGame.new
-game.play
-game.play_again
--- lib/game.rb
+++ lib/game.rb
@@ -1,19 +1,33 @@
-module Game
+module Util
   def input_to_index(user_input)
     user_input.to_i - 1
   end
+end
 
-  def move(board, index, player)
-    board[index] = player
+class Game
+  def initialize
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
   end
 
-  def valid_move?(board, index)
-    return true if index.between?(0, 8) && board[index] == ' '
+  attr_reader :board
+
+  def display_board
+    display = [
+    [" #{@board[0]} | #{@board[1]} | #{@board[2]} "],
+    ['----------- '],
+    [" #{@board[3]} | #{@board[4]} | #{@board[5]} "],
+    ['----------- '],
+    [" #{@board[6]} | #{@board[7]} | #{@board[8]} "]
+    ]
+  end
+
+  def valid_move?(index)
+    return true if index.between?(0, 8) && @board[index] == ' '
   end
 
   def turn_count(board)
     counter = 0
-    board.each do |spaces|
+    @board.each do |spaces|
       counter += 1 if spaces.include?('X') || spaces.include?('O')
     end
     counter
@@ -21,5 +35,33 @@ module Game
 
   def current_player
     turn_count(@board).even? ? 'X' : 'O'
+  end
+
+  def move(index, player)
+    @board[index] = player
+  end
+
+  def won?
+    if (@board[0] == @board[1] && @board[1] == @board[2]) && !@board.values_at(0, 1, 2).include?(' ')
+      true
+    elsif (@board[3] == @board[4] && @board[4] == @board[5]) && !@board.values_at(3, 4, 5).include?(' ')
+      true
+    elsif (@board[6] == @board[7] && @board[7] == @board[8]) && !@board.values_at(6, 7, 8).include?(' ')
+      true
+    elsif (@board[0] == @board[3] && @board[3] == @board[6]) && !@board.values_at(0, 3, 6).include?(' ')
+      true
+    elsif (@board[1] == @board[4] && @board[4] == @board[7]) && !@board.values_at(1, 4, 7).include?(' ')
+      true
+    elsif (@board[2] == @board[5] && @board[5] == @board[8]) && !@board.values_at(2, 5, 8).include?(' ')
+      true
+    elsif (@board[0] == @board[4] && @board[4] == @board[8]) && !@board.values_at(0, 4, 8).include?(' ')
+      true
+    elsif (@board[2] == @board[4] && @board[4] == @board[6]) && !@board.values_at(2, 4, 6).include?(' ')
+      true
+    end
+  end
+
+  def empty_board
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
   end
 end
